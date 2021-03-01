bd533619c1ba81622cb3812e38e8e643785a461f
Matthew Leingang <leingang@nyu.edu>
2021-03-01 16:14:23 -0500

    Add an option for the thumbnail size

--- ytimg/cli.py
+++ ytimg/cli.py
@@ -9,12 +9,13 @@ import ytimg.ytimg as ytimg
 
 # TODO: Add destination option
 @click.command()
+@click.option('--which',default='0',help='Which thumbnail to retrieve')
 @click.argument('url')
-def main(url):
+def main(url, which):
     """Console script for ytimg."""
     ytid = ytimg.youtube_id(url)
-    img_url = "http://img.youtube.com/vi/{}/0.jpg".format(ytid)
-    filename = "{}.jpg".format(ytid)
+    img_url = "http://img.youtube.com/vi/{}/{}.jpg".format(ytid,which)
+    filename = "{}_{}.jpg".format(ytid,which)
     response = requests.get(img_url)
     file = open(filename, "wb")
     file.write(response.content)
